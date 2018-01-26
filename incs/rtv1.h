6dd1f03a6ed803c0b213b32f771100ecda1770a3
Heloise BOUCHET <hbouchet@e1r3p20.42.fr>
2018-01-26 06:20:38 +0100

    MERGE

--- incs/rtv1.h
+++ incs/rtv1.h
@@ -6,7 +6,7 @@
 /*   By: tjeanner <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/08 18:01:28 by tjeanner          #+#    #+#             */
-/*   Updated: 2018/01/26 04:29:12 by tjeanner         ###   ########.fr       */
+/*   Updated: 2018/01/26 05:28:59 by hbouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,9 +92,11 @@ typedef struct			s_env
 	t_cam				cam;
 	t_lum				lum;
 	int					nb_obj;
+	int					nb_lum;
+	int					nb_cam;
 	int					state;
 	float				flou;
-	t_obj				objs[100];
+	t_obj				*objs;
 	t_ray				init_rays;
 	int					(*col_fcts[4])(t_ray *init_rays, t_obj obj);
 }						t_env;
--- srcs/useless_functions.c
+++ srcs/useless_functions.c
@@ -6,7 +6,7 @@
 /*   By: tjeanner <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/22 00:39:43 by tjeanner          #+#    #+#             */
-/*   Updated: 2018/01/26 04:29:06 by tjeanner         ###   ########.fr       */
+/*   Updated: 2018/01/26 05:24:06 by hbouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,7 @@ t_env	*fill_env(t_env *env)
 	obj.col.c.g = 0;
 	obj.col.c.b = 255;
 	obj.col.c.a = 0;
-	ft_memcpy(&env->objs[0], &obj, sizeof(t_obj));
+	//ft_memcpy(&env->objs[0], &obj, sizeof(t_obj));
 	obj.type = 'p';
 	obj.o.x = 1000;
 	obj.o.y = 0;
@@ -78,7 +78,7 @@ t_env	*fill_env(t_env *env)
 	obj.col.c.g = 0;
 	obj.col.c.b = 0;
 	obj.col.c.a = 0;
-	ft_memcpy(&env->objs[1], &obj, sizeof(t_obj));
+	//ft_memcpy(&env->objs[1], &obj, sizeof(t_obj));
 	obj.type = 'p';
 	obj.o.x = -1000;
 	obj.o.y = 0;
@@ -90,7 +90,7 @@ t_env	*fill_env(t_env *env)
 	obj.col.c.g = 0;
 	obj.col.c.b = 0;
 	obj.col.c.a = 0;
-	ft_memcpy(&env->objs[2], &obj, sizeof(t_obj));
+	//ft_memcpy(&env->objs[2], &obj, sizeof(t_obj));
 	obj.type = 'p';
 	obj.o.x = 0;
 	obj.o.y = 1000;
@@ -102,7 +102,7 @@ t_env	*fill_env(t_env *env)
 	obj.col.c.g = 255;
 	obj.col.c.b = 0;
 	obj.col.c.a = 0;
-	ft_memcpy(&env->objs[3], &obj, sizeof(t_obj));
+	//ft_memcpy(&env->objs[3], &obj, sizeof(t_obj));
 	obj.type = 'p';
 	obj.o.x = 0;
 	obj.o.y = -1000;
@@ -114,7 +114,7 @@ t_env	*fill_env(t_env *env)
 	obj.col.c.g = 255;
 	obj.col.c.b = 0;
 	obj.col.c.a = 0;
-	ft_memcpy(&env->objs[4], &obj, sizeof(t_obj));
+	//ft_memcpy(&env->objs[4], &obj, sizeof(t_obj));
 	obj.type = 't';
 	obj.radius = 100;
 	obj.o.x = 11;
@@ -127,7 +127,7 @@ t_env	*fill_env(t_env *env)
 	obj.col.c.g = 255;
 	obj.col.c.b = 255;
 	obj.col.c.a = 0;
-	ft_memcpy(&env->objs[5], &obj, sizeof(t_obj));
+	//ft_memcpy(&env->objs[5], &obj, sizeof(t_obj));
 	obj.type = 's';
 	obj.o.x = 0;
 	obj.o.y = 0;
@@ -137,7 +137,7 @@ t_env	*fill_env(t_env *env)
 	obj.col.c.g = 255;
 	obj.col.c.b = 255;
 	obj.col.c.a = 0;
-	ft_memcpy(&env->objs[6], &obj, sizeof(t_obj));
+	//ft_memcpy(&env->objs[6], &obj, sizeof(t_obj));
 	obj.type = 'c';
 	obj.radius = 5;
 	obj.o.x = -11;
@@ -150,7 +150,7 @@ t_env	*fill_env(t_env *env)
 	obj.col.c.g = 0;
 	obj.col.c.b = 255;
 	obj.col.c.a = 0;
-	ft_memcpy(&env->objs[7], &obj, sizeof(t_obj));
+	//ft_memcpy(&env->objs[7], &obj, sizeof(t_obj));
 //	if (!(env->init_rays = (t_ray *)malloc(sizeof(t_ray) * 1)))
 //		return (0);
 	env->nb_obj = 8;
