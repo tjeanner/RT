af8d364d53a34df154012f58e48c73683913bb4a
Tanguy JEANNERET <tjeanner@student.42.fr>
2018-01-27 03:19:47 +0100

    ADD: a get_next_line who doesn't segfault ;P

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -3,40 +3,69 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hbouchet <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tjeanner <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/01/20 17:27:10 by hbouchet          #+#    #+#             */
-/*   Updated: 2017/12/23 02:07:15 by hbouchet         ###   ########.fr       */
+/*   Created: 2017/10/10 11:39:00 by tjeanner          #+#    #+#             */
+/*   Updated: 2017/10/10 11:50:23 by tjeanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		get_next_line(const int fd, char **line)
+static char	*ft_read_line(int fd, char *str, int *eof, int *cpt)
 {
-	static char	*rest[OPEN_MAX];
-	t_gnl		gnl;
+	char	*buff;
+	char	*buff2;
 
-	gnl.ret = 1;
-	if (fd < 0 || !line || !BUFF_SIZE || fd > OPEN_MAX)
-		return (-1);
-	if (!rest[fd])
-		rest[fd] = ft_strnew(0);
-	while (gnl.ret > -1)
+	if ((buff = (char *)(malloc(sizeof(char) * (BUFF_SIZE + 1)))) == NULL)
+		return (NULL);
+	*eof = read(fd, buff, BUFF_SIZE);
+	*cpt += (*eof != 0) ? 1 : 0;
+	buff[*eof] = '\0';
+	buff2 = str;
+	str = ft_strjoin(str, buff);
+	ft_strdel(&buff2);
+	free(buff);
+	return (str);
+}
+
+static int	ft_get_line(char *tmp, char *p_tmp, char **line)
+{
+	if ((p_tmp = ft_strchr(tmp, '\n')))
 	{
-		if ((gnl.tmp = ft_strchr(rest[fd], '\n'))
-			|| ((gnl.tmp = ft_strchr(rest[fd], '\0')) && !gnl.ret))
-		{
-			*gnl.tmp = '\0';
-			*line = ft_strdup(rest[fd]);
-			ft_memmove(rest[fd], gnl.tmp + 1, ft_strlen(gnl.tmp + 1) + 1);
-			rest[fd] = (!gnl.ret) ? ft_strdel(&rest[fd]) : rest[fd];
-			return (!line[0][0] && gnl.ret == 0) ? 0 : 1;
-		}
-		if ((gnl.ret = read(fd, gnl.buf, BUFF_SIZE)) == -1)
-			return (-1);
-		gnl.buf[gnl.ret] = '\0';
-		rest[fd] = ft_strjoinfree(rest[fd], gnl.buf, 'L');
+		*p_tmp = '\0';
+		*line = ft_strdup(tmp);
+		ft_memmove(tmp, p_tmp + 1, ft_strlen(p_tmp + 1) + 1);
+		return (1);
 	}
-	return (-1);
+	return (0);
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	static char	*tmp = NULL;
+	char		*p_tmp;
+	int			eof;
+	int			cpt;
+
+	if ((cpt = 0) != -1 && (!line || fd < 0 || BUFF_SIZE < 1))
+		return (-1);
+	eof = 1;
+	p_tmp = NULL;
+	tmp = (tmp == NULL) ? ft_strnew(0) : tmp;
+	while (eof > 0)
+	{
+		if (ft_get_line(tmp, p_tmp, line))
+			return (1);
+		tmp = ft_read_line(fd, tmp, &eof, &cpt);
+	}
+	*line = ft_strdup(tmp);
+	if (ft_strlen(tmp) > 1 && eof == 0)
+	{
+		ft_memmove(tmp, "\0", ft_strlen(tmp));
+		return (1);
+	}
+	if (eof == -1)
+		return (-1);
+	return ((cpt != 0) ? 1 : 0);
 }
