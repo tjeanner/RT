5d48e557a181dcb42c8b42f08b86dcdb12830e58
Tanguy JEANNERET <tjeanner@student.42.fr>
2018-01-28 08:01:34 +0100

    REPAIR:[ft_strdup was segfaulting on av[1] because of a bad sizeof use]

--- libft/srcs/ft_strdup.c
+++ libft/srcs/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: hbouchet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 04:05:08 by hbouchet          #+#    #+#             */
-/*   Updated: 2017/09/03 04:18:29 by hbouchet         ###   ########.fr       */
+/*   Updated: 2018/01/28 07:59:37 by tjeanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strdup(const char *s1)
 {
 	char	*str;
 
-	str = (char*)malloc(sizeof(*str) * (ft_strlen(s1) + 1));
+	str = (char*)malloc(sizeof(char) * (ft_strlen(s1) + 1));
 	if (str == 0)
 		return (0);
 	str = ft_strcpy(str, s1);
