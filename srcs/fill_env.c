91b894e8b88d657fd6d8d108619ea5181c7db000
Heloise BOUCHET <hbouchet@e1r12p23.42.fr>
2018-02-02 04:02:21 +0100

    ADD : Rotation matrix

--- srcs/fill_env.c
+++ srcs/fill_env.c
@@ -6,11 +6,27 @@
 /*   By: hbouchet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/27 05:36:57 by hbouchet          #+#    #+#             */
-/*   Updated: 2018/02/01 22:30:21 by hbouchet         ###   ########.fr       */
+/*   Updated: 2018/02/02 03:59:19 by hbouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
+
+t_v			rotation(t_v n, t_v r, float t)
+{
+	double	c;
+	double	s;
+	t_v		v;
+
+	c = cos(t / 360 * 2 * M_PI);
+	s = sin(t / 360 * 2 * M_PI);
+	v.x = (r.x * r.x * (1 - c) + c) * n.x + (r.x * r.y * (1 - c) - r.x * s) * n.y
+		+ (n.x * n.z * (1 - c) + n.y * s) * n.z;
+	v.y = (r.x * r.y * (1 - c) + r.z * s) * n.x + (r.y * r.y * (1 - c) + c) * n.y
+		+ (r.y * r.z * (1 - c) - r.x * s) * n.z;
+	v.z = (r.x * r.z * (1 - c) - r.y * s) * n.x + (r.y * r.z * (1 - c) + r.x * s) * n.y + (r.z * r.z * (1 - c) + c) * n.z;
+	return (v);
+}
 
 int			is_norm_wchar(double x, double y, double z)
 {
@@ -75,13 +91,13 @@ int			is_valid_obj(char **str, int n_line)
 		return (0);
 	while (str[i])
 		i++;
-	if (i != 12)
+	if (i != 16)
 		return (putlineerr("invalid obj line ", n_line));
 	if (ft_strstr(str[0], "plane") && !is_norm_wchar(ft_atof(str[4]),
 		ft_atof(str[5]), ft_atof(str[6])))
 		return (putlineerr("invalid vector line (obj) ", n_line));
 	i = 0;
-	while (str[11][i])
+	while (str[15][i])
 		i++;
 	if (i < 6)
 		return (putlineerr("invalid color line ", n_line));
