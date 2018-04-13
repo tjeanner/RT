c79abbe6e38f8341c81c3104c81d8620aae517eb
Heloise BOUCHET <hbouchet@e1r9p15.42.fr>
2018-04-13 02:21:51 +0200

    FIX : ft_atof

--- libft/srcs/ft_atof.c
+++ libft/srcs/ft_atof.c
@@ -3,29 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atof.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tjeanner <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: hbouchet <hbouchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/26 01:24:43 by tjeanner          #+#    #+#             */
-/*   Updated: 2018/02/18 05:39:15 by tjeanner         ###   ########.fr       */
+/*   Updated: 2018/04/13 02:21:22 by hbouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 double		ft_atof(char *s)
 {
 	int		i;
+	int		len;
 	int		j;
 	int		neg;
 	double	res;
 
-	neg = 1;
-	res = (double)atoi(s);
-	if (ft_strchr(s, '.') && (i = ft_strchr(s, '.') - s) != 0)
+	neg = 0;
+	len = 0;
+	while ((s[++len] >= '0' && s[len] <= '9') || (s[len] == '.' && neg == 0))
+		if (s[len] == '.')
+			neg++;
+	res = (double)ft_atoi(s);
+	if (ft_strchr(s, '.') && (i = ft_strchr(s, '.') - s) != 0 && i < len)
 	{
 		j = 1;
-		if (s[0] == '-')
-			neg = -1;
+		neg = (s[0] == '-') ? -1 : 1;
 		while (s[++i] && ft_isdigit(s[i]))
 		{
 			res += (neg == 1) ? ((double)(s[i] - '0') * ft_pow(10, 0 - j)) :
@@ -33,5 +38,6 @@ double		ft_atof(char *s)
 			j++;
 		}
 	}
+	printf("%f\n", res);
 	return (res);
 }
