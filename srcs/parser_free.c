3299d770346f5600cde78b53d425d429b1a98156
Heloise BOUCHET <hbouchet@e1r3p21.42.fr>
2018-04-17 20:15:53 +0200

    MAJ: leaks parser

--- srcs/parser_free.c
+++ srcs/parser_free.c
@@ -6,7 +6,7 @@
 /*   By: hbouchet <hbouchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/14 02:37:14 by hbouchet          #+#    #+#             */
-/*   Updated: 2018/04/17 10:45:27 by hbouchet         ###   ########.fr       */
+/*   Updated: 2018/04/17 16:28:40 by hbouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,42 +17,38 @@ static void		ft_free_arr(t_json_arr *tab)
 	if (tab->next)
 	{
 		ft_free_arr(tab->next);
-//		ft_memdel((void **)&tab->next);
+		free(tab->next);
 	}
-	else
+	if (tab->val.data.obj->next)
 	{
-		if (tab->val.data.obj->next)
-			ft_parser_free(tab->val.data.obj->next);
-		else if (tab->val.data.obj)
-		{
-			ft_parser_free(tab->val.data.obj);
-//			free(tab->val.data.obj);
-		}
-//		free(tab);
+		ft_parser_free(tab->val.data.obj->next);
+		free(tab->val.data.obj->next);
+		tab->val.data.obj->next = NULL;
 	}
-//	if (tab)
-//		free(tab);
+	ft_parser_free(tab->val.data.obj);
+	free(tab->val.data.obj);
 }
 
 void		ft_parser_free(t_json *json)
 {
 	if (json->next)
-		ft_parser_free(json->next);
-	else
 	{
-//		printf("%s\n", json->key);
-		free(json->key);
-		if (json->val.type == TYPE_STRING)
-		{
-//			printf("> %s\n", json->val.data.str);
-			free(json->val.data.str);
-		}
-		else if (json->val.type == TYPE_ARR)
-			ft_free_arr(json->val.data.tab);
-		else if (json->val.type == TYPE_DOUBLE)
-			;
-		else if (json->val.type == TYPE_OBJ)
-			ft_parser_free(json->val.data.obj);
-//		free(json);
+		ft_parser_free(json->next);
+		free(json->next);
+	}
+	free(json->key);
+	if (json->val.type == TYPE_STRING)
+		free(json->val.data.str);
+	else if (json->val.type == TYPE_ARR)
+	{
+		ft_free_arr(json->val.data.tab);
+		free(json->val.data.tab);
+	}
+	else if (json->val.type == TYPE_DOUBLE)
+		;
+	else if (json->val.type == TYPE_OBJ)
+	{
+		ft_parser_free(json->val.data.obj);
+		free(json->val.data.obj);
 	}
 }
